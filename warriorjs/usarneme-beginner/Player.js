59caed4e3de9fd7a98dad140bf41cab18f7eeedf
usarneme <jamestcoop@gmail.com>
2018-05-19 14:39:58 -0700

    Refactored to include needsHealing flag. Will run away from archers until at a safe distance and then heal to full. Added logic to move to the left-most square (freeing captives and killing enemies on the way) before moving to the right (using a backwards toggle set to forward when the warrior gets to square 1,0). Passes the current level.

--- warriorjs/usarneme-beginner/Player.js
+++ warriorjs/usarneme-beginner/Player.js
@@ -1,7 +1,9 @@
 class Player {
   // this.health to track player's health
   constructor() {
-    this.health = 20
+    this.health = 20,
+    this.direction = 'backward'
+    this.needsHealing = false
   }
   
   // warrior.think(Object.keys(warrior.feel()))
@@ -15,58 +17,62 @@ class Player {
 
   playTurn(warrior) {
     warrior.think('Location: '+warrior.feel().getLocation())
-    warrior.think('Was: '+this.health+'. Is: '+warrior.health())    
+    warrior.think('loc[0]: '+warrior.feel().getLocation()[0])
+    warrior.think('Was: '+this.health+'. Is: '+warrior.health())
+
+    // always start by moving to the left-most room
+    if (warrior.feel().getLocation()[0] > 1 && this.direction == 'backward') {
+      // check to the left for a captive or enemy unit
+      if (warrior.feel('backward').isUnit()) {
+        // if it's a friendly
+        if (warrior.feel('backward').getUnit().isFriendly()) {
+          // ...rescue the captive
+          warrior.rescue('backward')
+          this.health = warrior.health() 
+          return
+        } else {
+          // ...otherwise attack the enemy
+          warrior.attack('backward')
+          this.health = warrior.health() 
+          return
+        }
+      } else {
+        warrior.walk('backward')
+        this.health = warrior.health() 
+        return
+      }
+    }
+
+    // Once the left-most area has been cleared, reset the direction to forward
+    if (warrior.feel().getLocation()[0] == 1) {
+      this.direction = 'forward'
+    }
 
     // Rest/Health Logic
-    // If I am low health AND not under archer attack
+    // If I am low health AND NOT under archer attack
     // ...I should run away and rest
 
     // If I am low health 
-    if (warrior.health() < 15) {
-      // if there is an archer shooting me
-      // ...move forward, rescue captive, or attack enemy
+    if (warrior.health() < 11) {
+      this.needsHealing = true
+    }
 
+    if (this.needsHealing) {
       // If my health is decreasing (i.e.: I am under archer attack)
       if (this.health > warrior.health()) {
-        // if the adjacent room has a unit
-        if (warrior.feel().isUnit()) {
-          // assigned for ease of reading code
-          const unit = warrior.feel().getUnit()
-          warrior.think('unit ahead. \tFriendly: '+unit.isFriendly()+'.\tPlayer: '+unit.isPlayer()+'.\t Warrior: '+unit.isWarrior()+'.\tBound: '+unit.isBound()+'\tUnder Effect: '+unit.isUnderEffect()+'.')
-          // if the adjacent room unit is a friend and bound
-          if (unit.isFriendly() && unit.isBound()) {
-            warrior.rescue()
-            this.health = warrior.health() 
-            return
-          } else {
-            // The adjacent room unit is not friendly
-            warrior.attack()
-            this.health = warrior.health() 
-            return  
-          }
-        } else {
-          // the adjacent room does not have a unit
-          // ..walk towards the archer doing damage
-          warrior.walk() 
-          this.health = warrior.health()
-          return
-        }    
-      }
-      // ...therefore there is no archer
-      // if there is no unit in the next square
-      if (!warrior.feel().isUnit()) {
-        // safe to heal to full
-        if(this.health < 20) {
-          warrior.rest()
-          this.health = warrior.health()
-          return  
-        }
-      } else {
-        // there is a unit in the next square...run away
+        // ...run away
         warrior.walk('backward')
         this.health = warrior.health()
         return
       }
+      // ...therefore there is no archer
+      // ...therefore it is safe to heal to full
+      warrior.rest()
+      this.health = warrior.health()
+      if (warrior.health() == 20) {
+        this.needsHealing = false
+      }
+      return  
     }
 
     // Adjacent unit Logic (full health)
